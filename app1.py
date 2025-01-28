90279178c83a5e15cb4c82056cb08d7b29e44ab6
Abdulla-basil <abdullabasil1256@gmail.com>
2025-01-28 14:55:12 +0530

    Add files via upload

--- app1.py
+++ app1.py
@@ -2,6 +2,7 @@ import pickle
 import streamlit as st
 import pandas as pd
 import numpy as np
+pip install scikit-learn
 from sklearn.preprocessing import StandardScaler 
 from sklearn.tree import DecisionTreeClassifier
 
